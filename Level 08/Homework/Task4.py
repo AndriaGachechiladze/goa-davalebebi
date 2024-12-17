2b556416fa02b69c2c44c0902dd74bd13121bb21
AndriaGachechiladze <andrewandria999i@gmail.com>
2024-12-17 17:34:46 +0400

    commit

---
+++ Level 08/Homework/Task4.py
@@ -0,0 +1,7 @@
+"""4)მომხმარებელს შემოატანინეთ სახელი გვარი ასაკი სიმაღლე საყვარელი ფერი და გამოიტანეთ ეს ყველაფერი ერთ დიდ წინადადებაში."""
+name=input("Enter Your Name: ")
+last_name=input("Enter Your Surname: ")
+Age=input("Enter Your Age: ")
+Height=input("Enter Your Height: ")
+Favcolor=input("Enter Your Favorite Color: ")
+print(name + " " + last_name + ", you are " + Age +  " Years old And Your height is " + Height + " Your favorite color is " + Favcolor)
---
+++ Level 08/Homework/task1.py
@@ -0,0 +1,6 @@
+"""1)ჩაწერე განსხვავებული მონაცემთა ტიპების (int, float, string, boolean) ცვლადები."""
+
+number1=10
+number2=10.5
+name="Andria"
+programer=True
